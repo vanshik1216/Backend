8b549535c206b5400057c8d7fc339cc020d658bd
Vanshika <gargvanshika430@gmail.com>
2025-09-16 14:11:13 +0530

    Modified

--- LECTURE28/index.js
+++ LECTURE28/index.js
@@ -1,23 +1,149 @@
-const {PrismaClient}=require('./generated/prisma');
+const { PrismaClient } = require('./generated/prisma');
 const prisma = new PrismaClient();
-async function addUser(name,email,password){
-await prisma.user.create({
-    data:{
-        name:name,
-        email:email,
-        password:password
-    }
-})
-}
+// async function addUser(name,email,password){
+// await prisma.user.create({
+//     data:{
+//         name:name,
+//         email:email,
+//         password:password
+//     }
+// })
+// }
 // addUser("g","v@gmail.com","123")
 // .then(()=>{
 //     console.log("User added successfully!!!")
 // })
-async function getAllUser(){
-    let allUsers=await prisma.user.findMany();
-    return allUsers;
+// async function getAllUser(){
+//     let allUsers=await prisma.user.findMany();
+//     return allUsers;
+// }
+// getAllUser()
+// .then((data)=>{
+//     console.log(data)
+// })
+async function addData() {
+    await prisma.user.createMany({
+        data: [
+            {
+                name: "abc",
+                email: "abc@gmail.com",
+                password: "abc"
+            },
+            {
+                name: "a",
+                email: "a@gmail.com",
+                password: "a"
+            }
+        ]
+    })
 }
-getAllUser()
+// addData()
+// .then(()=>{
+//     console.log("data added")
+// })
+//getby unique identifier or id
+//  async function get(){
+//     return await  prisma.user.findUnique({
+//         where:{
+//             email:'v@gmail.com'
+//         }
+//     })
+// }
+// get()
+// .then((data)=>{
+//     console.log(data)
+// })
+//get all records
+// async function getAll(){
+//     let data=await prisma.user.findMany();
+//     return data;
+// }
+// getAll()
+// .then((data)=>{
+//     console.log(data)
+// })
+//getting all data by filtering
+// async function getBy (){
+//    return await prisma.user.findMany({
+//         where:{
+//             password:{
+//                 startsWith:"123"
+//             }
+//         }
+//     })
+// }
+// getBy()
+// .then((data)=>{
+//     console.log(data)
+// })
+//findMany mein hum boht sare func use krskte hai where,select,skip ,take,include,orderBy
+// async function getWhere(){
+//     return await prisma.user.findMany({
+//        where:{
+//         name:{
+//             contains:"v"
+//         }
+//        } 
+//     })
+// }
+// getWhere()
+// .then((data)=>{
+//     console.log(data)
+// })
+// async function select(){
+//    return await prisma.user.findMany({
+//     select: {
+//       id: true,
+//       email: true
+//     }
+//   })
+// }
+// select()
+// .then((data)=>{
+//     console.log(data)
+// })
+//update -- to update single record
+// async function update(){
+//     await prisma.user.update({
+//         where:{
+//             email:"vg@gmail.com"
+//         },
+//         data:{
+//             name:"vg"
+//         }
+//     })
+// }
+// update()
+// .then(()=>{
+//     console.log("Updated data succesfully")
+// })
+//update many
+// async function updateMany(){
+// await  prisma.user.updateMany({
+//     where:{
+//         email:{
+//             contains:"v"
+//         }
+//     },
+//     data:{
+//         password:"new"
+//     }
+// })
+// }updateMany()
+//updatemany andreturn used to update the query and to return the resulting obj
+ async function updatemany(){
+    return await prisma.user.updateManyAndReturn({
+        where: {
+            email: {
+                contains: "v"
+            }
+        },
+        data: {
+            password:"123"
+        }
+    })
+}
+updatemany()
 .then((data)=>{
     console.log(data)
 })
